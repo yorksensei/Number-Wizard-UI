1df034bae11ecd8ed1a570362e112ee520f7085a
yorksensei <yorksensei@gmail.com>
2018-01-23 21:09:08 +0900

    Added buttons to the Start and Game Scenes

---
+++ Assets/LevelManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel(string scene) {
+		Debug.Log("Level load requested for " + scene);
+		SceneManager.LoadScene (scene, LoadSceneMode.Single);
+		
+	}
+
+	public void QuitRequest() {
+		Debug.Log("Quit requested");
+
+	}
+}
