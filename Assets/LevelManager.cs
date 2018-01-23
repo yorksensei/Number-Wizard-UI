ef63d2fdd9c6c637b9005d013a362af95a624406
yorksensei <yorksensei@gmail.com>
2018-01-23 21:43:59 +0900

    Created some more buttons and added images

--- Assets/LevelManager.cs
+++ Assets/LevelManager.cs
@@ -7,12 +7,11 @@ public class LevelManager : MonoBehaviour {
 
 	public void LoadLevel(string scene) {
 		Debug.Log("Level load requested for " + scene);
-		SceneManager.LoadScene (scene, LoadSceneMode.Single);
-		
+		SceneManager.LoadScene (scene, LoadSceneMode.Single);	
 	}
 
 	public void QuitRequest() {
 		Debug.Log("Quit requested");
-
+		Application.Quit ();
 	}
 }
